8bb1204745dd91c84e5396be776d5a98a81352fd
Danny <51312422+dchenz@users.noreply.github.com>
2026-08-22 20:04:49 +1000

    Speed up generation by using multiple processes

--- generate.py
+++ generate.py
@@ -1,5 +1,7 @@
 import argparse
 import itertools
+import os
+from concurrent.futures import Future, ProcessPoolExecutor
 from typing import Iterator
 
 
@@ -26,39 +28,48 @@ def var(v: int) -> str:
     return chr(ord("a") + v)
 
 
-def processPerms(n: int, arg: int) -> str:
-    sumExprParts: list[str] = []
-    for i in range(n):
-        orConditionParts: list[str] = []
-        for lessThan, greaterThan in getPerms(n, arg):
-            andConditionParts: list[str] = []
-            for v in lessThan:
-                v += i - arg
-                v %= n
-                andConditionParts.append(f"{var(v)} <= {var(i)}")
-            for v in greaterThan:
-                v += i - arg
-                v %= n
-                andConditionParts.append(f"{var(i)} <= {var(v)}")
-            if andConditionParts:
-                andCondition = " && ".join(andConditionParts)
-                orConditionParts.append(andCondition)
-        if len(orConditionParts) > 1:
-            orConditionParts = [f"({part})" for part in orConditionParts]
-        if orConditionParts:
-            orCondition = f"({' || '.join(orConditionParts)})"
-            multiplyCondition = f"{orCondition} * {var(i)}"
-        else:
-            multiplyCondition = var(i)
-        dupeGuardParts: list[str] = []
-        for j in range(0, i):
-            dupeGuardParts.append(f"{var(i)} != {var(j)}")
-        dupeGuard = " && ".join(dupeGuardParts)
-        if dupeGuard:
-            multiplyCondition = f"{multiplyCondition} * ({dupeGuard})"
-        sumExprParts.append(multiplyCondition)
-    sumExpr = " + ".join(sumExprParts)
-    return sumExpr
+def getMultiplyCondition(n: int, arg: int, i: int) -> str:
+    orConditionParts: list[str] = []
+    for lessThan, greaterThan in getPerms(n, arg):
+        andConditionParts: list[str] = []
+        for v in lessThan:
+            v += i - arg
+            v %= n
+            andConditionParts.append(f"{var(v)} <= {var(i)}")
+        for v in greaterThan:
+            v += i - arg
+            v %= n
+            andConditionParts.append(f"{var(i)} <= {var(v)}")
+        if andConditionParts:
+            andCondition = " && ".join(andConditionParts)
+            orConditionParts.append(andCondition)
+    if len(orConditionParts) > 1:
+        orConditionParts = [f"({part})" for part in orConditionParts]
+    if orConditionParts:
+        orCondition = f"({' || '.join(orConditionParts)})"
+        multiplyCondition = f"{orCondition} * {var(i)}"
+    else:
+        multiplyCondition = var(i)
+    dupeGuardParts: list[str] = []
+    for j in range(0, i):
+        dupeGuardParts.append(f"{var(i)} != {var(j)}")
+    dupeGuard = " && ".join(dupeGuardParts)
+    if dupeGuard:
+        multiplyCondition = f"{multiplyCondition} * ({dupeGuard})"
+    return multiplyCondition
+
+
+def getArgExpression(n: int) -> Iterator[str]:
+    cpuCount = os.cpu_count() or 1
+    with ProcessPoolExecutor(max_workers=max(1, cpuCount - 2)) as executor:
+        sumExprParts: list[Future[str]] = []
+        for arg in range(n):
+            for i in range(n):
+                sumExprParts.append(executor.submit(getMultiplyCondition, n, arg, i))
+        for i in range(n):
+            yield "        " + " + ".join(
+                sumExpr.result() for sumExpr in sumExprParts[i * n : (i + 1) * n]
+            )
 
 
 def main():
@@ -66,14 +77,10 @@ def main():
 
     n = args.n
 
-    argExprParts: list[str] = []
-    for arg in range(n):
-        argExprParts.append("        " + processPerms(n, arg))
-    argExpr = ",\n".join(argExprParts)
-
     formatExpr = " ".join("%d" for _ in range(n))
     varsExpr = ", ".join(f"{var(v)}" for v in range(n))
     varsPointersExpr = ", ".join(f"&{var(v)}" for v in range(n))
+    argExpr = ",\n".join(getArgExpression(n))
 
     prog = f"""#include <stdio.h>
 
